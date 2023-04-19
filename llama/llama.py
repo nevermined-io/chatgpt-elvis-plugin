bfadf317b658a5d8d8b7c8a7b570333f53c09de2
enrique <enrique@keyko.io>
2023-04-19 13:50:32 +0200

    feat: integrate endpoint in server

--- llama/llama.py
+++ llama/llama.py
@@ -1,6 +1,7 @@
 from llama_index.readers import ChatGPTRetrievalPluginReader
 from llama_index.indices.vector_store import ChatGPTRetrievalPluginIndex
 import os
+from llama_index import GPTSimpleVectorIndex
 
 
 # load documents
@@ -10,16 +11,23 @@ reader = ChatGPTRetrievalPluginReader(
     endpoint_url="http://localhost:8000",
     bearer_token=bearer_token
 )
-documents = reader.load_data("*")
+documents = reader.load_data("Adam and Evil,", top_k=1, separate_documents=True)
 
 
-print(len(documents))
 
-index = ChatGPTRetrievalPluginIndex.from_documents(
-    documents,
-    endpoint_url="http://localhost:8000",
-    bearer_token=bearer_token,
-)
+# print(documents)
+
+# index = ChatGPTRetrievalPluginIndex.from_documents(
+#     documents,
+#     endpoint_url="http://localhost:8000",
+#     bearer_token=bearer_token,
+# )
+
+index = GPTSimpleVectorIndex.from_documents(documents)
+# index.save_to_disk('index.json')
+
+# index = GPTSimpleVectorIndex.load_from_disk('index.json')
+
 
 #query index
 response = index.query("Summarize the content of the song.",  response_mode='compact')
